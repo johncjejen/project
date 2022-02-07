5a8a4100d441fdaf64da2c6dd194bf5409fca963
johncjejen <johncjejen@hotmail.com>
2022-02-06 22:13:53 -0500

    se integra inicio on google

--- Gemfile
+++ Gemfile
@@ -24,6 +24,9 @@ gem 'jbuilder', '~> 2.7'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'devise'
 gem 'bootstrap', '~> 4.1'
+gem "dotenv-rails"
+gem 'omniauth-google-oauth2'
+gem "omniauth-rails_csrf_protection"
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  #devise_for :users, :controllers => { registrations: 'users/registrations' }
+  #devise_for :users, path: "auth", :controllers => {sessions: 'sessions', registrations: 'registrations', confirmations: 'confirmations', passwords: 'passwords', omniauth_callbacks: 'omniauth_callbacks'} 
+  get "index/index"
+
+  root :to => "index#index"
+
 end
